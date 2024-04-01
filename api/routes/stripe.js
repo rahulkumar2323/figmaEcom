e56dd1629ae247bb91f1084a347edc5fb821b3e8
rahulkumar <107376311+flashpogu@users.noreply.github.com>
2024-04-01 22:49:18 +0530

    added success and cancel url

--- api/routes/stripe.js
+++ api/routes/stripe.js
@@ -20,8 +20,8 @@ router.post("/payment", async (req, res) => {
       payment_method_types: ["card"],
       line_items,
       mode: "payment",
-      success_url: "http://localhost:5173/success",
-      cancel_url: "http://localhost:5173/cancel",
+      success_url: "https://figmaecom.onrender.com/success",
+      cancel_url: "https://figmaecom.onrender.com/cancel",
     });
     res.json({ id: session.id });
   } catch (error) {
